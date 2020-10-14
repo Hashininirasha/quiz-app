14ac6a0ce57d860254d283ae8d64882fd25d2a26
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-10-14 21:57:20 +0530

    push array

---
+++ app.js
@@ -0,0 +1,6 @@
+
+
+const question_number=document.querySelecter(",question_number");
+const question_text=document.querySelecter(",question_text");
+const question_container=document.querySelecter(",option-container");
+
