13c591fc7dbf216e9577aba39c3b8288088393d3
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-10-14 22:08:03 +0530

    push array

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ const question_number=document.querySelecter(",question_number");
 const question_text=document.querySelecter(",question_text");
 const question_container=document.querySelecter(",option-container");
 
+let questioncounter=0;
+let currentquestions;
+let availablequestions=[]
+
+function setavailableQ(){
+	const totalQ=quiz.length;
+	for(let i=0;i<totalQ;i++){
+		availablequestions.push(quiz[i])
+	}
+function getnewQ(){
+	question_number.innerHTML="Question"+(questioncounter+1)+ "of" +quiz.length;
+
+
+
+}
+
+
+}
+window.onload=function(){
+	setavailableQ();
+	getnewQ()
+
+
+}
